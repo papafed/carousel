e07a8a4a19de182365cdb8a6d70305ad93d656f1
Niall King <n.king@syzygy.co.uk>
2019-05-06 11:45:34 +0100

    add Comments and mobile viewport meta

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -24,6 +24,7 @@ This will return an object with a `hits` property, which will be an array of ima
   const container = document.querySelector('.carousel-list');
   const errorContainer = document.querySelector('.show-on-error');
   const controlsContainer = document.querySelector('.carousel-controls');
+  // these values are set in the CSS.
   const marginWidth = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--imageMargin'),10) * 2;
   const imageWidth = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--imageWidth'),10) + marginWidth;
   
@@ -33,7 +34,7 @@ This will return an object with a `hits` property, which will be an array of ima
    
     appContainer.style.width='';
     const width=appContainer.getBoundingClientRect().width;
-    // how many images are completely visible on the screen?
+    // how many images are completely visible on the screen? Resize container to fit.
      appContainer.style.width = (Math.floor(width / imageWidth) * imageWidth) + 'px';
   }
 
@@ -42,7 +43,10 @@ This will return an object with a `hits` property, which will be an array of ima
   }
 
   function renderImage({id, small, tags, fullSize}) {
-    
+    // while the spec said have a central active image the design contradicts that
+    // by giving every image equal prominence.
+    // and you can't have a central image if there is an even number visible 
+    // Therefore lets make all images active, and link to a bigger version of themselves.
     const img = document.createElement('IMG');
     img.src=small;
     img.alt=`${tags}`;
@@ -63,6 +67,7 @@ This will return an object with a `hits` property, which will be an array of ima
   }
 
   function navigate(direction) {
+    // rather than slide, let's just re-order the array and re-render the lot.
     let toAdd = null;
     switch (direction) {
       case 'prev':
@@ -74,10 +79,11 @@ This will return an object with a `hits` property, which will be an array of ima
       images.push(toAdd);
         break;
     } 
-
+    // remove all the images
     while (container.firstChild) {
       container.removeChild(container.firstChild);
     }
+    // and render them again.
     renderImages();
   }
 
@@ -102,10 +108,7 @@ This will return an object with a `hits` property, which will be an array of ima
                 height : hit.webformatHeight,
                 width : hit.webformatWidth,
                 fullSize : hit.largeImageURL,
-                fullHeight : hit.imageHeight,
-                fullWidth : hit.imageWidth,
-                tags : hit.tags,
-                user : hit.user,
+                tags : hit.tags
               });
             });
             renderImages();
